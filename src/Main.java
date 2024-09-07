51668d0594767b3c59c5dbfbce02c20154801c4a
Lucas Neves <lucas.ccdr@gmail.com>
2024-09-07 12:32:02 -0300

    ajuste para numero de tentativas

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-//Crie um programa que simula um jogo de adivinhação, que deve gerar um número aleatório entre 0 e 100 e pedir para que o usuário tente adivinhar o número, em até 5 tentativas. A cada tentativa, o programa deve informar se o número digitado pelo usuário é maior ou menor do que o número gerado.
+//Crie um programa que simula um jogo de adivinhação,
+// que deve gerar um número aleatório entre 0 e 100 e
+// pedir para que o usuário tente adivinhar o número, em até 5 tentativas.
+// A cada tentativa, o programa deve informar se o número digitado pelo usuário é maior ou menor
+//do que o número gerado.
 //
 //Dicas:
 //
@@ -8,9 +12,27 @@
 //Utilize um loop para controlar as tentativas;
 //Utilize a instrução break; para interromper o loop.
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int numeroAleatorio = new Random().nextInt(100);
+        int tentativa = 0;
+        int chute = 0;
+        Scanner leitura = new Scanner(System.in);
+
+
+
+        while (tentativa <=4) {
+            System.out.println("Adivinhe um número de 0 a 100.");
+            tentativa ++;
+            chute = leitura.nextInt();
+
+            }
+
+        }
     }
-}
+
 
